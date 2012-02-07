40494fef19d8ce221fc574c75f2952a0e024ecb6
Antonio Hernández <ahdiaz@gmail.com>
2012-02-07 21:33:27 +0100

    Update the test.

--- tests/test.php
+++ tests/test.php
@@ -1,34 +1,51 @@
 <?php
 
-dl(../php_cexp.so);
-
-function test_ifnull($value, $null_value) {
-	$r = ifnull($value, $null_value);
-	var_dump($value);
-	echo ">>\n";
-	var_dump($r);
-	echo "\n";
+function value_to_string($value) {
+    if (is_null($value)) {
+        return 'NULL';
+    } else if ($value === true) {
+        return 'TRUE';
+    } else if ($value === FALSE) {
+        return 'FALSE';
+    } else if (is_array($value)) {
+        return 'ARRAY';
+    } else if (is_numeric($value)) {
+        return "NUMERIC ($value)";
+    } else if (is_string($value)) {
+        return "STRING ($value)";
+    }
 }
 
-function test_ifempty($value, $null_value) {
-	$r = ifempty($value, $null_value);
-	var_dump($value);
-	echo ">>\n";
-	var_dump($r);
-	echo "\n";
+function test_ifnull($value, $value_if_null) {
+    $r = ifnull($value, $value_if_null);
+    if ($r === $value_if_null) {
+        printf(" > '%s' is NULL\n", value_to_string($value));
+    } else {
+        printf(" > '%s' is NOT NULL\n", value_to_string($value));
+    }
 }
 
-$values = array(null, 'NOT NULL', '', 0, 0.0, true, false, array());
-$is_null = 'is null';
-$is_empty = 'is empty';
-
-echo "--- ifnull() ---\n";
-foreach ($values as $value) {
-	test_ifnull($value, $is_null);
+function test_ifempty($value, $value_if_empty) {
+    $r = ifempty($value, $value_if_empty);
+    if ($r === $value_if_empty) {
+        printf(" > '%s' is EMPTY\n", value_to_string($value));
+    } else {
+        printf(" > '%s' is NOT EMPTY\n", value_to_string($value));
+    }
 }
 
-echo "--- ifempty() ---\n";
-foreach ($values as $value) {
-	test_ifempty($value, $is_empty);
+function test() {
+    $values = array(null, 'NOT NULL', '', 0, 0.0, 1, true, false, array());
+
+    echo "--- Testing ifnull() ---\n";
+    foreach ($values as $value) {
+        test_ifnull($value, 'VALUE_IF_NULL');
+    }
+
+    echo "\n--- Testing ifempty() ---\n";
+    foreach ($values as $value) {
+        test_ifempty($value, 'VALUE_IF_EMPTY');
+    }
 }
 
+test();
